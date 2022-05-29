67bc7e2a6eb6dd92f60c65ac7773607a2fbb75bc
JosueLC <josueflopez@msn.com>
2022-05-28 20:34:33 -0500

    completed exercise 2

--- assets/js/curso-practico.js
+++ assets/js/curso-practico.js
@@ -47,3 +47,57 @@ function calcularCirculo() {
     document.getElementById("circPerim").innerText = perimetro.toFixed(2);
     document.getElementById("circArea").innerText = area.toFixed(2);
 }
+
+/* Ejercicio 2: Promedio, Mediana y Moda */
+function generateRandomArray() {
+    let array = [];
+    for (let i = 0; i < 10; i++) {
+        array.push(Math.floor(Math.random() * 100));
+    }
+    return array;
+}
+function calcularPromedio(array) {
+    return array.reduce((a, b) => a + b) / array.length;
+}
+function calcularMediana(array) {
+    array.sort((a, b) => a - b);
+    if (array.length % 2 == 0) {
+        return (array[array.length / 2] + array[array.length / 2 - 1]) / 2;
+    }
+    return array[Math.floor(array.length / 2)];
+}
+function calcularModa(array) {
+    let moda = 0;
+    let max = 0;
+    for (let i = 0; i < array.length; i++) {
+        let count = 0;
+        for (let j = 0; j < array.length; j++) {
+            if (array[i] == array[j]) {
+                count++;
+            }
+        }
+        if (count > max) {
+            max = count;
+            moda = array[i];
+        }
+    }
+    return moda;
+}
+function calcularPromedioBtn(){
+    let list = document.getElementById("promNums");
+    let array = (list.value === "" ? list.placeholder : list.value ).split(",").map(x => parseFloat(x));
+    let promedio = calcularPromedio(array);
+    document.getElementById("promedio").innerText = promedio.toFixed(2);
+}
+function calcularMedianaBtn(){
+    let list = document.getElementById("medNums");
+    let array = (list.value === "" ? list.placeholder : list.value ).split(",").map(x => parseFloat(x));
+    let mediana = calcularMediana(array);
+    document.getElementById("mediana").innerText = mediana.toFixed(2);
+}
+function calcularModaBtn(){
+    let list = document.getElementById("modNums");
+    let array = (list.value === "" ? list.placeholder : list.value ).split(",").map(x => parseFloat(x));
+    let moda = calcularModa(array);
+    document.getElementById("moda").innerText = moda.toFixed(2);
+}
